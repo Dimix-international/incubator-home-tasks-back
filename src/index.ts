270f387e884b2e2a8d363087d703708ba61fde1a
Дмитрий Мельников <dima.dimix94@mail.ru>
2022-10-12 09:01:59 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { videosRouter } from "./routes/videos-router";
 import {allDataRouter} from "./routes/all_data-router";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const parserMiddleware = bodyParser({});
 
