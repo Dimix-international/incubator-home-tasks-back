3ac84958051943ac954b16e192fe060ef2db0cdb
Дмитрий Мельников <dima.dimix94@mail.ru>
2022-10-18 21:16:15 +0300

    refactor trim

--- src/validator-schemas/blogs-validator-schema.ts
+++ src/validator-schemas/blogs-validator-schema.ts
@@ -3,20 +3,20 @@ import {urlFormat} from "../constants/general/general";
 
 export const BlogsValidatorSchema = [
     body('name')
-        .exists({checkFalsy: true})
-        .withMessage('This field is required!')
         .isString()
         .withMessage('Incorrect data format!')
         .trim()
-        .isLength({max: 15, min: 1})
+        .exists({checkFalsy: true})
+        .withMessage('This field is required!')
+        .isLength({max: 15})
         .withMessage('Max 15 symbols'),
     body('youtubeUrl')
-        .exists({checkFalsy: true})
-        .withMessage('This field is required!')
         .isString()
         .withMessage('Incorrect data format!')
         .trim()
-        .isLength({max: 100,  min: 1})
+        .exists({checkFalsy: true})
+        .withMessage('This field is required!')
+        .isLength({max: 100})
         .withMessage('Max 100 symbols')
         .matches(urlFormat)
         .withMessage('Incorrect url address!'),
--- src/validator-schemas/post-validator-schema.ts
+++ src/validator-schemas/post-validator-schema.ts
@@ -3,34 +3,35 @@ import {BlogsRepository} from "../repositories/blogs-repository";
 
 export const PostValidatorSchema = [
     body('title')
-        .exists({checkFalsy: true})
-        .withMessage('This field is required!')
         .isString()
         .withMessage('Incorrect data format!')
         .trim()
-        .isLength({max: 30,  min: 1})
+        .exists({checkFalsy: true})
+        .withMessage('This field is required!')
+        .isLength({max: 30})
         .withMessage('Max 30 symbols'),
     body('shortDescription')
-        .exists({checkFalsy: true})
-        .withMessage('This field is required!')
         .isString()
         .withMessage('Incorrect data format!')
         .trim()
-        .isLength({max: 100,  min: 1})
+        .exists({checkFalsy: true})
+        .withMessage('This field is required!')
+        .isLength({max: 100})
         .withMessage('Max 100 symbols'),
     body('content')
-        .exists({checkFalsy: true})
-        .withMessage('This field is required!')
         .isString()
         .withMessage('Incorrect data format!')
         .trim()
-        .isLength({max: 1000,  min: 1})
-        .withMessage('Max 1000 symbols'),
-    body('blogId')
         .exists({checkFalsy: true})
         .withMessage('This field is required!')
+        .isLength({max: 1000})
+        .withMessage('Max 1000 symbols'),
+    body('blogId')
         .isString()
         .withMessage('Incorrect data format!')
+        .trim()
+        .exists({checkFalsy: true})
+        .withMessage('This field is required!')
         .custom((value) => {
             const searchedBlog = BlogsRepository.getBlogById(value);
             if (!searchedBlog) {
