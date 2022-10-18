ef7b3e71788113c1a330d6ee86ea2f584f28f5ba
Дмитрий Мельников <dima.dimix94@mail.ru>
2022-10-18 21:23:37 +0300

    auth

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -12,7 +12,7 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
 
     try {
 
-        const token = req.headers.authorization?.split(' ')[1];
+        const token = req.headers?.authorization?.split(' ')[1];
 
         if (!token) {
             res.send(HTTP_STATUSES.UNAUTHORIZED_401);
