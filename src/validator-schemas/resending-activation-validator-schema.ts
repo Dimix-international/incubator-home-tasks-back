b3db0bff1be413cd4e914007a9bd4d7a2e3c0750
Дмитрий Мельников <dima.dimix94@mail.ru>
2022-11-23 21:00:23 +0300

    refactor

--- src/validator-schemas/resending-activation-validator-schema.ts
+++ src/validator-schemas/resending-activation-validator-schema.ts
@@ -13,8 +13,8 @@ export const ResendingActivationValidatorSchema = [
         .trim()
         .exists({checkFalsy: true})
         .withMessage('This field is required!')
-       // .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
-       // .withMessage('Incorrect email format!')
+        .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+        .withMessage('Incorrect email format!')
         .custom( async (_, {req}) => {
             const {body: { email }} = req as RequestWithBody<ResendingViewModel>;
 
